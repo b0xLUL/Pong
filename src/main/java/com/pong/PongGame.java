c586bbe9f3cdcf9208aa46dfd4c2e519bcd5633f
Mels <djslememeel@gmail.com>
2023-03-24 13:08:05 +0100

    functionality for populating a settings file if there isnt already one

--- src/main/java/com/pong/PongGame.java
+++ src/main/java/com/pong/PongGame.java
@@ -6,8 +6,17 @@ import com.pong.scenes.GameLevel;
 import com.pong.scenes.OptionsMenu;
 import com.pong.scenes.PauseMenu;
 import com.pong.scenes.TitleScene;
+import com.pong.shared.defaults.DefaultSettings;
+import com.pong.shared.util.FileHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PongGame extends YaegerGame {
+
+    public DefaultSettings Settings;
+    private static FileHandler settingFile;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -16,6 +25,13 @@ public class PongGame extends YaegerGame {
     public void setupGame() {
         setSize(new Size(800, 600));
         setGameTitle("Pong");
+
+        settingFile = new FileHandler("settings.txt");
+
+        if(!settingFile.FileExists()) {
+            Settings = new DefaultSettings();
+            setupFiles(settingFile, Settings.toString());
+        }
     }
 
     @Override
@@ -24,5 +40,13 @@ public class PongGame extends YaegerGame {
         addScene(1, new GameLevel());
         addScene(2, new OptionsMenu());
         addScene(3, new PauseMenu());
+    }
+
+    private void setupFiles(FileHandler setupFile, String defaultValues) {
+        String[] splittedValues = defaultValues.split(" ");
+        ArrayList<String> valueList = new ArrayList<String>(Arrays.asList(splittedValues));
+
+        setupFile.CreateFile();
+        setupFile.WriteFile(valueList);
     }
 }
