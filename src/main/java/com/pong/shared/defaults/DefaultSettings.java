70aed279016f890fb540e16a05f528094e26fb0e
Mels <djslememeel@gmail.com>
2023-03-24 13:12:50 +0100

    whitespace rm

--- src/main/java/com/pong/shared/defaults/DefaultSettings.java
+++ src/main/java/com/pong/shared/defaults/DefaultSettings.java
@@ -5,7 +5,7 @@ package com.pong.shared.defaults;
  * for the game.
  * @author Mels Ubbink
  */
-public record DefaultSettings( ) {
+public record DefaultSettings() {
     private static final double DefaultVolume = 100;
 
     @Override
