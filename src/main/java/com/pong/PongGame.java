f974d9b0798994973dc3b2c709aa2be9aeac43f3
Mels <djslememeel@gmail.com>
2023-03-24 14:53:52 +0100

    added functionality to read from the record files and write to the records

--- src/main/java/com/pong/PongGame.java
+++ src/main/java/com/pong/PongGame.java
@@ -16,8 +16,6 @@ import java.util.Arrays;
 public class PongGame extends YaegerGame {
     public Settings SettingsRecord;
     public Score ScoreRecord;
-    private static FileHandler settingFile;
-    private static FileHandler scoreFile;
 
     public static void main(String[] args) {
         launch(args);
@@ -28,21 +26,21 @@ public class PongGame extends YaegerGame {
         setSize(new Size(800, 600));
         setGameTitle("Pong");
 
-        settingFile = new FileHandler("settings.txt");
-        scoreFile = new FileHandler("score.txt");
+        FileHandler settingFile = new FileHandler("settings.txt");
+        FileHandler scoreFile = new FileHandler("score.txt");
         SettingsRecord = new Settings();
         ScoreRecord = new Score();
 
         if (!settingFile.FileExists()) {
             setupFiles(settingFile, SettingsRecord.getDefaultVolume());
         } else {
-            readToRecord(SettingsRecord, settingFile);
+            readToSettingsRecord(SettingsRecord, settingFile);
         }
 
         if(!scoreFile.FileExists()) {
             setupFiles(scoreFile, ScoreRecord.getDefaultScores());
         } else {
-            readToRecord(ScoreRecord, scoreFile);
+            readToScoreRecord(ScoreRecord, scoreFile);
         }
     }
 
@@ -54,13 +52,40 @@ public class PongGame extends YaegerGame {
         addScene(3, new PauseMenu());
     }
 
-    private void readToRecord(Record record, FileHandler recordFile) {
+    private void readToSettingsRecord(Settings record, FileHandler recordFile) {
         System.out.println(recordFile.getFileName() + " already exists, writing existing settings to record..");
+        ArrayList<String> readOutLines = recordFile.ReadFile();
+
+        for(String dataInString : readOutLines) {
+            double dataInDouble = Double.parseDouble(dataInString);
+            record.setVolume(dataInDouble);
+        }
     }
+
+    private void readToScoreRecord(Score record, FileHandler recordFile) {
+        System.out.println(recordFile.getFileName() + " already exists, writing existing settings to record..");
+        ArrayList<String> readOutLines = recordFile.ReadFile();
+
+        //0 = Player
+        //1 = computer
+        //2 = player-highscore
+        int[] dataInInt = new int[3];
+
+        int i = 0;
+        for(String dataInString : readOutLines) {
+            dataInInt[i] = Integer.parseInt(dataInString);
+            i++;
+        }
+
+        record.setPlayerScore(dataInInt[0]);
+        record.setComputerScore(dataInInt[1]);
+        record.setHighScore(dataInInt[2]);
+    }
+
 
     private void setupFiles(FileHandler setupFile, String defaultValues) {
         String[] splittedValues = defaultValues.split(" ");
-        ArrayList<String> valueList = new ArrayList<String>(Arrays.asList(splittedValues));
+        ArrayList<String> valueList = new ArrayList<>(Arrays.asList(splittedValues));
 
         setupFile.CreateFile();
         setupFile.WriteFile(valueList);
