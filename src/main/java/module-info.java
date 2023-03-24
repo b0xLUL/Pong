f83bd8bc89b71a086cc3d58385685f50295a4bae
Mels <djslememeel@gmail.com>
2023-03-24 14:10:09 +0100

    export added for the shared.records

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,7 @@ module com.pong {
     exports com.pong.entities.buttons;
     exports com.pong.entities;
     exports com.pong.shared.buttons;
-    exports com.pong.shared.defaults;
+    exports com.pong.shared.records;
 
 //    opens audio;
 //    opens entities;
